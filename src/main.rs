b849a5c1cca5bea96ead38d3c936c99cbfeb47bf
Robsutar <robsonsutar@gmail.com>
2024-11-18 07:07:08 -0300

    Add base physics engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::default();
+    app.add_plugins(DefaultPlugins);
+    app.add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0));
+    app.add_plugins(RapierDebugRenderPlugin::default());
+    app.add_systems(Startup, setup);
+    app.run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(500.0, 50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
 }

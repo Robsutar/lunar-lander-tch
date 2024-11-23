00c4db18c75d3ef8029ed0e75be29fca15c3f42e
Robsutar <robsonsutar@gmail.com>
2024-11-23 19:23:02 -0300

    Add ExperienceConcat

--- src/model.rs
+++ src/model.rs
@@ -25,3 +25,49 @@ pub struct Experience {
     /// A boolean flag that indicates if the episode has terminated.
     pub done: bool,
 }
+
+pub struct ExperienceConcat {
+    pub state: Vec<DType>,
+    pub action: Vec<DType>,
+    pub reward: Vec<DType>,
+    pub next_state: Vec<DType>,
+    pub done: Vec<DType>,
+    target_size: usize,
+}
+impl ExperienceConcat {
+    pub fn building(target_size: usize) -> Self {
+        Self {
+            state: Vec::with_capacity(target_size * State::SIZE),
+            action: Vec::with_capacity(target_size * StepActionEvent::SIZE),
+            reward: Vec::with_capacity(target_size),
+            next_state: Vec::with_capacity(target_size * State::SIZE),
+            done: Vec::with_capacity(target_size),
+            target_size,
+        }
+    }
+
+    pub fn push(&mut self, experience: &Experience) {
+        if self.is_built() {
+            panic!("ExperienceConcat already built (full).");
+        }
+        self.state.extend_from_slice(&experience.state.0);
+        self.action.push(experience.action.to_index() as DType);
+        self.reward.push(experience.reward);
+        self.next_state.extend_from_slice(&experience.next_state.0);
+        self.done.push(if experience.done { 1.0 } else { 0.0 });
+    }
+
+    pub fn is_built(&self) -> bool {
+        self.done.len() == self.target_size
+    }
+
+    pub fn check_built(&self) {
+        if !self.is_built() {
+            panic!(
+                "ExperienceConcat is not built, filled: {:?}, target size: {:?}",
+                self.done.len(),
+                self.target_size
+            );
+        }
+    }
+}

512dc4cfe77d049b36b5e6765f4b75de94870a63
Robsutar <robsonsutar@gmail.com>
2024-11-23 19:22:48 -0300

    Add Experience struct

--- src/model.rs
+++ src/model.rs
@@ -5,3 +5,23 @@ use crate::{State, StepActionEvent};
 
 const DEVICE: Device = Device::Cpu;
 type DType = f32;
+
+/// Represents an experience tuple (𝑆𝑡, 𝐴𝑡, 𝑅𝑡, 𝑆𝑡+1) used in Deep Q-Network (DQN) algorithms.
+///
+/// This struct captures the agent's interaction with the environment in reinforcement learning.
+/// It stores the current state (`state`), the action taken (`action`), the reward received (`reward`),
+/// and the next state (`next_state`) after the action is applied. Additionally, the `done` flag indicates
+/// whether the episode has terminated.
+#[derive(Debug)]
+pub struct Experience {
+    /// The state 𝑆𝑡 before the action is taken.
+    pub state: State,
+    /// The action 𝐴𝑡 performed by the agent.
+    pub action: StepActionEvent,
+    /// The reward 𝑅𝑡 received after taking the action.
+    pub reward: DType,
+    /// The resulting state 𝑆𝑡+1 after the action is applied.
+    pub next_state: State,
+    /// A boolean flag that indicates if the episode has terminated.
+    pub done: bool,
+}

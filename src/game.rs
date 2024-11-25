7378b9f58a160466df602017758bedd89fda3d81
Robsutar <robsonsutar@gmail.com>
2024-11-25 14:19:11 -0300

    Remove min angle limit from legs
    This allow them to rotate to inside the module, witch is a ugly effect, but this solves the problem of falling with high speed, but with one leg, avoiding the damage.

--- src/game.rs
+++ src/game.rs
@@ -573,9 +573,9 @@ fn game_init(
         let leg_translation = Vec2::new(i * LEG_AWAY / SCALE, 0.0);
 
         let joint_limits = if i == -1.0 {
-            [-0.9 - leg_angle, 0.0]
+            [-0.9 - leg_angle, 0.5 + leg_angle]
         } else {
-            [0.0, 0.9 + leg_angle]
+            [-0.5 - leg_angle, 0.9 + leg_angle]
         };
 
         let leg_id = commands

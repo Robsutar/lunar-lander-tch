cc8f2481bee54dbf5608accfd76bcb02b22f1222
Robsutar <robsonsutar@gmail.com>
2024-11-25 16:34:52 -0300

    Show GameGraph state in ui

--- src/graph.rs
+++ src/graph.rs
@@ -1,5 +1,9 @@
 use crate::{controller_ai, controller_human, game::*};
 use bevy::prelude::*;
+use bevy_egui::{
+    egui::{self},
+    EguiContexts, EguiPlugin,
+};
 
 struct GameGraph {
     state: State,
@@ -18,6 +22,8 @@ pub struct GraphPlugin;
 
 impl Plugin for GraphPlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins(EguiPlugin);
+
         app.add_systems(
             PostGameResetSchedule,
             game_post_reset
@@ -36,6 +42,8 @@ impl Plugin for GraphPlugin {
                 .before(controller_ai::game_post_step)
                 .before(controller_human::game_post_step),
         );
+
+        app.add_systems(Update, ui_update);
     }
 }
 
@@ -75,6 +83,31 @@ fn game_post_step(mut graph: ResMut<Graph>, mut ev_step_result: EventReader<Step
     graph.actual_game.state = next_state;
 
     graph.actual_game.total_points += reward;
+}
 
-    println!("Post step!");
+fn ui_update(graph: Res<Graph>, mut egui_context: EguiContexts) {
+    egui::Window::new("State")
+        .resizable(false)
+        .show(egui_context.ctx_mut(), |ui| {
+            let state = &graph.actual_game.state;
+
+            ui.label(format!("position_x: {:.5}", state.position_x()));
+            ui.label(format!("position_y: {:.5}", state.position_y()));
+            ui.label(format!("velocity_x: {:.5}", state.velocity_x()));
+            ui.label(format!("velocity_y: {:.5}", state.velocity_y()));
+            ui.label(format!(
+                "orientation_angle: {:.5}",
+                state.orientation_angle()
+            ));
+            ui.label(format!("angular_velocity: {:.5}", state.angular_velocity()));
+
+            ui.label(format!(
+                "is_left_leg_contact: {:?}",
+                state.is_left_leg_contact()
+            ));
+            ui.label(format!(
+                "is_right_leg_contact: {:?}",
+                state.is_right_leg_contact()
+            ));
+        });
 }

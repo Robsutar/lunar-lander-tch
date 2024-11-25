bd23c9bdcc0946def770f10a0444159139442ce8
Robsutar <robsonsutar@gmail.com>
2024-11-25 16:29:04 -0300

    Register state in GameGraph

--- src/graph.rs
+++ src/graph.rs
@@ -2,6 +2,8 @@ use crate::{controller_ai, controller_human, game::*};
 use bevy::prelude::*;
 
 struct GameGraph {
+    state: State,
+
     total_points: f32,
 }
 
@@ -48,12 +50,18 @@ fn game_post_reset(
         Some(mut graph) => {
             graph.num_games += 1;
 
-            graph.actual_game = GameGraph { total_points: 0.0 };
+            graph.actual_game = GameGraph {
+                state,
+                total_points: 0.0,
+            };
         }
         None => {
             commands.insert_resource(Graph {
                 num_games: 0,
-                actual_game: GameGraph { total_points: 0.0 },
+                actual_game: GameGraph {
+                    state,
+                    total_points: 0.0,
+                },
             });
         }
     }
@@ -63,6 +71,8 @@ fn game_pre_step() {}
 
 fn game_post_step(mut graph: ResMut<Graph>, mut ev_step_result: EventReader<StepResultEvent>) {
     let (next_state, reward, done) = ev_step_result.read().next().unwrap().clone().unpack();
+
+    graph.actual_game.state = next_state;
 
     graph.actual_game.total_points += reward;
 

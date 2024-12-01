6a7c9ad3acbd641d49a4bb6de97be40804bdbc3c
Robsutar <robsonsutar@gmail.com>
2024-12-01 09:36:40 -0300

    Add Agent doc

--- src/agent.rs
+++ src/agent.rs
@@ -27,6 +27,7 @@ const E_DECAY: f64 = 0.995;
 /// Minimum ε value for the ε-greedy policy.
 const E_MIN: f64 = 0.01;
 
+/// Used to train the model trainer (DDqnTrainer), with experience replay and ε-greedy policy.
 pub struct Agent {
     memory_buffer: ExperienceReplayBuffer,
     trainer: DDqnTrainer,
